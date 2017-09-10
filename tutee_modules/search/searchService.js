1744aa1eedd6cdf5a4ce5f6993ffe0899efa29f1
Alessandro-Commodari <commodari.a@gmail.com>
2017-09-10 14:32:42 -0400

    Made search api, fixed eslint, fixed SOME postTests, and app.js session fix

--- tutee_modules/search/searchService.js
+++ tutee_modules/search/searchService.js
@@ -3,43 +3,41 @@ var tagService = require('../tag/tagService.js');
 
 exports.searchPosts = function(searchTags) {
     var parsedTags = exports.parseSearchTags(searchTags);
-    var pidList;
     var postList;
     return tagService.getPidList(parsedTags, function(pids) {
-        //console.log(pids);
+        // console.log(pids);
         return postService.getPostList(pids).then(function(list) {
             postList = list;
-            //console.log(postList);
+            // console.log(postList);
             return postList;
         });
     });
-}
-
+};
 
 exports.parseSearchTags = function(searchString) {
-    //checks for null or undefined tags
+    // checks for null or undefined tags
     if (searchString === null || searchString === undefined) {
         return null;
     }
-    //deals with any speical characters and also checks if user used #s
-    if(/[^a-zA-Z0-9\s#]/g.test(searchString)) {
+    // deals with any speical characters and also checks if user used #s
+    if (/[^a-zA-Z0-9\s#]/g.test(searchString)) {
         return null;
     }
-    //gets rid of whitespaces and replace with #
-    searchString = searchString.replace(/\s/g, "#");
-    //makes everything lowercase for consistency and deals with starting # so that first element of array isn't empty
+    // gets rid of whitespaces and replace with #
+    searchString = searchString.replace(/\s/g, '#');
+    // makes everything lowercase for consistency and deals with starting # so that first element of array isn't empty
     searchString = searchString.toLowerCase();
-    var tempTags = "#" + searchString;
-    //deals with instances of multiple consecutive hashtags
+    var tempTags = '#' + searchString;
+    // deals with instances of multiple consecutive hashtags
     if (/#{2,}/g.test(tempTags)) {
-        tempTags = tempTags.replace(/#{2,}/g, "#");
+        tempTags = tempTags.replace(/#{2,}/g, '#');
     }
-    //deals with string that ends with 1 or more #s
+    // deals with string that ends with 1 or more #s
     if (/#+$/g.test(tempTags)) {
-        tempTags = tempTags.replace(/#+$/g,"");
+        tempTags = tempTags.replace(/#+$/g, '');
     }
     if (tempTags === '') {
         return null;
     }
     return tempTags;
-}
+};
