d9d4754a7a3ea5d53cf583142c4e2431ac8f37cb
VittorioG <vittorio.mcgill@gmail.com>
2017-09-10 20:39:01 -0400

    Created endpoints for messaging

--- routes/messaging.js
+++ routes/messaging.js
@@ -3,13 +3,15 @@ var router = express.Router();
 var messagingService = require('../tutee_modules/messaging/messagingService.js');
 
 router.post('/send', function(req, res, next) {
-    res.send(messagingService.sendMessage(req.body.uidFrom, req.body.uidTutor, req.body.uidTutee, req.body.content));
+  messagingService.sendMessage(req.body.uidFrom, req.body.uidTutor, req.body.uidTutee, req.body.content, function(message) {
+    res.send(message);
+  });
 });
 
 router.get('/get/:uidTutor/:uidTutee', function(req, res, next) {
-    messagingService.getMessages(req.params.uidTutor, req.params.uidTutee).then(function(message) {
-        res.send(message);
-    });
+  messagingService.getMessages(req.params.uidTutor, req.params.uidTutee).then(function(message) {
+    res.send(message);
+  });
 });
 
 module.exports = router;
--- tutee_modules/messaging/messagingService.js
+++ tutee_modules/messaging/messagingService.js
@@ -1,34 +1,35 @@
 var firebase = require('firebase');
 
-exports.sendMessage = function(uidFrom, uidTutor, uidTutee, content) {
-    firebase.database().ref('messages/' + uidTutor + '-' + uidTutee).transaction(function(currentMsg) {
-        if (currentMsg === null) {
-            var newDate = new Date();
-            console.log(newDate);
-            var message = {
-              tutor: uidTutor,
-              tutee: uidTutee,
-              messages: [{sentBy: uidFrom, content: content, date: newDate}]
-            };
-            return message;
-        } else {
-            currentMsg.messages.push({sentBy: uidFrom, content: content, date: newDate});
-            return currentMsg;
-        }
-    }, function(error, committed, snapshot) {
-        if (error) {
-            console.log('Transaction failed abnormally!', error);
-        } else {
-            console.log('Message Sent');
-            console.log(snapshot.val());
-            return snapshot.val();
-        }
-        console.log('The message looks like this: ', snapshot.val());
-    });
+exports.sendMessage = function(uidFrom, uidTutor, uidTutee, content, callback) {
+  firebase.database().ref('messages/' + uidTutor + '-' + uidTutee).transaction(function(currentMsg) {
+    if (currentMsg === null) {
+      var newDate = new Date();
+      // console.log(newDate);
+      dateString = newDate.toString();
+      var message = {
+        tutor: uidTutor,
+        tutee: uidTutee,
+        messages: [{sentBy: uidFrom, content: content, date: dateString}]
+      };
+      return message;
+    } else {
+      currentMsg.messages.push({sentBy: uidFrom, content: content, date: dateString});
+      return currentMsg;
+    }
+  }, function(error, committed, snapshot) {
+    if (error) {
+      // console.log('Transaction failed abnormally!', error);
+      callback(error);
+    } else {
+      // console.log('Message Sent');
+      // console.log(snapshot.val());
+      callback(snapshot.val().messages[snapshot.val().messages.length-1]);
+    }
+  });
 };
 
 exports.getMessages = function(uidTutor, uidTutee) {
-    return firebase.database().ref('messages/' +uidTutor + '-' + uidTutee).once('value').then(function(snapshot) {
-        return snapshot.val().messages;
-    });
+  return firebase.database().ref('messages/' +uidTutor + '-' + uidTutee).once('value').then(function(snapshot) {
+    return snapshot.val().messages;
+  });
 };
